2300c5c7c6e37879820d36c180d7d6679a9a1da5
Yukee-798 <909397116@qq.com>
2021-04-15 12:16:59 +0800

    fix: limit upload file counts

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,24 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Row, Col, Button } from 'antd'
-import { DragDropContext, Draggable } from 'react-beautiful-dnd'
-import {
-  MenuFoldOutlined,
-  MenuUnfoldOutlined
-} from '@ant-design/icons';
-import './App.scss';
+import { MenuFoldOutlined } from '@ant-design/icons';
 import Displayer from './components/Displayer/displayer';
 import Console from './components/Console/console';
 import domtoimage from 'dom-to-image';
 import { IFile } from './types';
 import { saveAs } from 'file-saver';
+import './App.scss';
 
 const { ipcRenderer } = window.require('electron');
-ipcRenderer.on('reply', (event: any, args: any) => {
-  console.log(args);
-})
 
 function App() {
 
   const [isPopConsole, setIsPopConsole] = useState(false);
   const [picList, setPicList] = useState<Pick<IFile, 'name' | 'path'>[]>([]);
 
-
-
   return (
     <div className="App">
       <Row>
         <Col span={24}>
-          {/* <DragDropContext
-            onDragEnd={(r, p) => {console.log(r, p);}}
-          > */}
           <Displayer
             loadPic={picList}
           />
@@ -57,29 +45,25 @@ function App() {
 
             // 当文件数量达到2个后，继续上传文件不会再执行回调
             onFileUpload={(file) => {
+
               const mFile = {
                 path: file.path,
                 name: file.name
               }
 
-              setPicList(pre => {
-                ipcRenderer.send('cacheFile', JSON.stringify({
-                  picList,
-                  file: mFile
-                }));
-                return [...pre, mFile]
-              });
+              if (picList.length < 2)
+                setPicList(pre => {
+                  ipcRenderer.send('cacheFile', JSON.stringify({
+                    picList,
+                    file: mFile
+                  }));
+                  return [...pre, mFile]
+                });
+
             }}
+
+
           />
-
-          {/* <Draggable
-              draggableId='123'
-              
-            >
-              <div id='123'>231</div>
-            </Draggable> */}
-
-
           <Button
             className='float-btn'
             icon={<MenuFoldOutlined />}
@@ -88,9 +72,6 @@ function App() {
             }}
           >
           </Button>
-
-
-          {/* </DragDropContext> */}
         </Col>
       </Row>
     </div>
